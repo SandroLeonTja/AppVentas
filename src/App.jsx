e0ca6edaeefc22acc4217d8f108b82496cd5ba70
LeonDev-0 <zandrotja@gmail.com>
2024-02-20 13:14:52 -0400

    agregacion de imagen con racion a 90 grados

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ function App() {
   return (
     <>
       <Header />
+      <div>
+        <img className="transition-transform duration-300 hover:rotate-90"src="https://img.freepik.com/vector-gratis/payaso-feliz-que-hace-juegos-malabares-bolas-blanco_1308-40992.jpg?size=626&ext=jpg" alt="" />
+      </div>
     </>
   );
 }
