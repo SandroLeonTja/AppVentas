02469677170f059efaaf651d6f902d8340716d3d
SandroLeonTja <virusznot@gmail.com>
2024-02-19 21:21:16 -0400

    se modifico app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./componentes/Header";
 function App() {
   return (
-    <>  
-    <Header />
-    <p>holaj</p>
-    <h1>agregado desde leondev-0</h1>
+    <>
+      <Header />
+      <p>holaj</p>
+      <h1>agregado desde leondev-0</h1>
+
+      <h2>nueva modificacion</h2>
     </>
   );
 }
